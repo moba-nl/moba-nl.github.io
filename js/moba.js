23738acfb257e84df61ac0ec9a72008ba4b52f24
Max van Kampen <max.van.kampen@alicon.nl>
2021-05-21 10:49:27 +0200

    Auto commit

---
+++ js/moba.js
@@ -0,0 +1,13 @@
+$().config({
+  client_id: "2347355-b989733c-b1f7-4471-9ddc-ad81220080bc",
+  info: {
+    title: "MOBA",
+  },
+  client: {
+    servers: [
+      {
+        url: "https://moba.aliconnect.nl/api"
+      }
+    ]
+  },
+})
